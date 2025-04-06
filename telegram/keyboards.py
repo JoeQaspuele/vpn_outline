2e803d15a5c07aece55c09de86c859e17a46cea4
JoeQaspuele <112169301+JoeQaspuele@users.noreply.github.com>
2025-04-07 00:29:56 +0700

    Update keyboards.py

--- telegram/keyboards.py
+++ telegram/keyboards.py
@@ -2,7 +2,7 @@ from telebot import types
 from telegram.messages import Buttons
 
 
-def main_menu() -> types.ReplyKeyboardMarkup:
+def main_menu(is_admin=False) -> types.ReplyKeyboardMarkup:
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(
         types.KeyboardButton(Buttons.GET_KEY),
@@ -12,7 +12,13 @@ def main_menu() -> types.ReplyKeyboardMarkup:
         types.KeyboardButton(Buttons.DONATE),
         types.KeyboardButton(Buttons.PREMIUM)
     )
+    
+    # Добавляем кнопку администратора только если пользователь администратор
+    if is_admin:
+        markup.add(types.KeyboardButton(Buttons.ADMIN))
+    
     return markup
+
 
 
 def support_cancel_markup() -> types.ReplyKeyboardMarkup:
