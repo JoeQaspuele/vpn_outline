42ae1f1d8bc7463a2162834cdb76ff512b85d0cf
JoeQaspuele <112169301+JoeQaspuele@users.noreply.github.com>
2025-04-07 17:31:59 +0700

    Update keyboards.py

--- telegram/keyboards.py
+++ telegram/keyboards.py
@@ -8,6 +8,7 @@ def main_menu() -> types.ReplyKeyboardMarkup:
         types.KeyboardButton(Buttons.GET_KEY),
         types.KeyboardButton(Buttons.MY_KEY),
         types.KeyboardButton(Buttons.DOWNLOAD),
+        types.KeyboardButton(Buttons.PREMIUM),
         types.KeyboardButton(Buttons.SUPPORT),
         types.KeyboardButton(Buttons.DONATE)
     )
@@ -18,3 +19,13 @@ def support_cancel_markup() -> types.ReplyKeyboardMarkup:
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(types.KeyboardButton(Buttons.CANCEL))
     return markup
+
+def premium_menu() -> types.ReplyKeyboardMarkup:
+    """Меню PREMIUM с кнопками"""
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(
+        types.KeyboardButton(Buttons.BUY_PREMIUM),
+        types.KeyboardButton(Buttons.BACK)
+    )
+    return markup
+
